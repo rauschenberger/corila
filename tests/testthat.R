f733ee3072f5410aa3bc7b6b613e94e6fd3d3019
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2025-10-01 09:08:18 +0200

    initialise unit tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(corila)
+
+testthat::test_check(package="corila",stop_on_warning=FALSE)
---
+++ tests/testthat/test.R
@@ -0,0 +1,4 @@
+
+testthat::test_that("1+1=1",{
+  testthat::expect_true(TRUE)
+})
