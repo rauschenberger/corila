b16049d92503ff49984dc1d7cb928c5b20c2dd90
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-07-06 16:18:14 +0200

    replace expect_true by expect_gt

--- tests/testthat/test-pkgname.R
+++ tests/testthat/test-pkgname.R
@@ -2,5 +2,5 @@
 testthat::test_that("help file for corila-package exists", {
   info <- ?corila::`corila-package`
   testthat::expect_type(object = info, type = "list")
-  testthat::expect_true(length(info) > 0)
+  testthat::expect_gt(object = length(info), expected = 0)
 })
