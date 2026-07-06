e50629846eb4c2ab6dfcbc460346ea0d896cd2a3
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-07-06 15:58:02 +0200

    add unit test for data

---
+++ tests/testthat/test-data.R
@@ -0,0 +1,34 @@
+
+data(data, package = "corila")
+
+slots <- c("x_train", "y_train", "group", "primary", "beta", "x_test", "y_test")
+
+testthat::test_that("list data contains expected slots",{
+  testthat::expect_named(object = data, expected = slots)
+})
+
+testthat::test_that("slots have expected types",{
+  testthat::expect_type(object = data$x_train, type = "double")
+  testthat::expect_type(object = data$y_train, type = "double")
+  testthat::expect_type(object = data$group, type = "integer")
+  testthat::expect_type(object = data$primary, type = "logical")
+  testthat::expect_type(object = data$beta, type = "double")
+  testthat::expect_type(object = data$x_test, type = "double")
+  testthat::expect_type(object = data$x_test, type = "double")
+})
+
+testthat::test_that("data have same number of rows (observations)",{
+  testthat::expect_identical(object = nrow(data$x_train),
+                             expected = length(data$y_train))
+  testthat::expect_identical(object = nrow(data$x_test),
+                             expected = length(data$y_test))
+})
+
+testthat::test_that("data have same number of columns (predictors)",{
+  testthat::expect_identical(object = ncol(data$x_test),
+                             expected = ncol(data$x_train))
+  testthat::expect_identical(object = length(data$primary),
+                             expected = ncol(data$x_train))
+  testthat::expect_identical(object = length(data$group),
+                             expected = ncol(data$x_train))
+})
