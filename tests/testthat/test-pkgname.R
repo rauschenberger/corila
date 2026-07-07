3c71e22333a1c3cc69badf1a8b6a34fcc88e9c22
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-07-07 11:47:35 +0200

    remove type test for info (due to difference between local and remote test)

--- tests/testthat/test-pkgname.R
+++ tests/testthat/test-pkgname.R
@@ -1,6 +1,6 @@
 
 testthat::test_that("help file for corila-package exists", {
   info <- ?corila::`corila-package`
-  testthat::expect_type(object = info, type = "list")
+  #testthat::expect_type(object = info, type = "character")
   testthat::expect_gt(object = length(info), expected = 0)
 })
