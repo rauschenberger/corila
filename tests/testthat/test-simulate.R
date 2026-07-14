80822f6e9f1aeff978c049705270cf8dc7936381
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-07-14 11:02:22 +0200

    add unit tests for function .simulate_outcome

---
+++ tests/testthat/test-simulate.R
@@ -0,0 +1,38 @@
+
+family <- c("gaussian", "binomial", "poisson", "cox")
+n <- 10
+p <- 5
+for(i in seq_along(family)) {
+  for(j in 1:2) {
+    if(j == 1) {
+      y <- .simulate_outcome(n = n, family = family[i])
+    } else {
+      x <- matrix(data = stats::rnorm(n = n * p), nrow = n, ncol = p)
+      beta <- stats::rnorm(n = p)
+      y <- .simulate_outcome(x = x, beta = beta, family = family[i])
+    }
+    testthat::test_that("function errors as expected", {
+      testthat::expect_error(object = .simulate_outcome(),
+                             regexp = "is missing")
+      testthat::expect_error(object = .simulate_outcome(family = "gamma"),
+                             regexp = "inside support")
+      testthat::expect_error(object = .simulate_outcome(family = family[i]),
+                             regexp = "Provide either")
+      testthat::expect_error(object = .simulate_outcome(x = x,
+                                                        beta = beta[-1],
+                                                        family = family[i]),
+                             regexp = "other length")
+    })
+    testthat::test_that("simulated outcome is numeric", {
+      testthat::skip_if_not(family[i] %in% c("gaussian", "cox"))
+      testthat::expect_type(object = y, type = "double")
+    })
+    testthat::test_that("simulated outcome is integer", {
+      testthat::skip_if_not(family[i] %in% c("poisson", "binomial"))
+      testthat::expect_type(object = y, type = "integer")
+    })
+    testthat::test_that("simulated outcome has expected length", {
+      testthat::expect_length(object = y, n = n)
+    })
+  }
+}
