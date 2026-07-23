833723424a5cd9d9bf27b48573057a7527b4ab79
Armin Rauschenberger <armin.rauschenberger@lih.lu>
2026-07-23 07:37:34 +0200

    move tests from test-data to test-simulate, test whether either none or both of x and beta are provided

--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -1,4 +1,6 @@
 
+#----- function .simulate_response ---------------------------------------------
+
 family <- c("gaussian", "binomial", "poisson", "cox")
 n <- 10L
 p <- 5L
@@ -23,6 +25,14 @@ for (i in seq_along(family)) {
                                                          beta = beta[-1L],
                                                          family = family[i]),
                              regexp = "other length")
+      testthat::expect_error(
+        object = .simulate_response(x = NULL, beta = beta, family = family[i]),
+        regexp = "either none or both"
+      )
+      testthat::expect_error(
+        object = .simulate_response(x = x, beta = NULL, family = family[i]),
+        regexp = "either none or both"
+      )
     })
     testthat::test_that("simulated outcome is numeric", {
       testthat::skip_if_not(family[i] %in% c("gaussian", "cox"))
@@ -37,3 +47,58 @@ for (i in seq_along(family)) {
     })
   }
 }
+
+#----- function simulate_data --------------------------------------------------
+
+data <- simulate_data()
+
+slots <- c("x_train", "y_train", "group", "primary", "beta", "x_test", "y_test")
+
+testthat::test_that("list data contains expected slots", {
+  testthat::expect_named(object = data, expected = slots)
+})
+
+testthat::test_that("slots have expected types", {
+  testthat::expect_type(object = data$x_train, type = "double")
+  testthat::expect_type(object = data$y_train, type = "double")
+  testthat::expect_type(object = data$group, type = "integer")
+  testthat::expect_type(object = data$primary, type = "logical")
+  testthat::expect_type(object = data$beta, type = "double")
+  testthat::expect_type(object = data$x_test, type = "double")
+  testthat::expect_type(object = data$x_test, type = "double")
+})
+
+testthat::test_that("data have same number of rows (observations)", {
+  testthat::expect_identical(object = nrow(data$x_train),
+                             expected = length(data$y_train))
+  testthat::expect_identical(object = nrow(data$x_test),
+                             expected = length(data$y_test))
+})
+
+testthat::test_that("data have same number of columns (predictors)", {
+  testthat::expect_identical(object = ncol(data$x_test),
+                             expected = ncol(data$x_train))
+  testthat::expect_length(object = data$primary,
+                          n = ncol(data$x_train))
+  testthat::expect_length(object = data$group,
+                          n = ncol(data$x_train))
+})
+
+testthat::test_that("data have consistent row names", {
+  testthat::expect_named(object = data$y_train,
+                         expected = rownames(data$x_train))
+  testthat::expect_named(object = data$y_test,
+                         expected = rownames(data$x_test))
+})
+
+testthat::test_that("data have consistent column names", {
+  colnames <- colnames(data$x_train)
+  testthat::expect_named(object = data$group,
+                         expected = colnames)
+  testthat::expect_named(object = data$primary,
+                         expected = colnames)
+  testthat::expect_identical(object = colnames(data$x_test),
+                             expected = colnames)
+  testthat::expect_named(object = data$beta,
+                         expected = colnames)
+})
